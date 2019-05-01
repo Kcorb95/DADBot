5e99863b317f400633e4f65d7a1509851abfd49a
AkiraKokiri <skittlesc123@gmail.com>
2019-05-01 18:28:41 -0400

    LED added directly to board, decided to go with one and only one button for controlling screen. Comments added, pretty much complete.

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,20 +1,23 @@
 const { Event } = require('klasa');
-const LCD = require('../lib/structures/LCD');
+const LCD = require('../lib/structures/LCD'); // LCD class
 
 module.exports = class extends Event {
-    async run(member) {
-        const lcd = LCD.getLCD();
+    async run(member) { // This event runs on the event where a new user joins the guild (server). It by default passes a member and client object we can use
+        const lcd = LCD.getLCD(); // Get our LCD object
+        const led = LCD.getLED(); // Get our LED object
 
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print(`New Member!`);
-        lcd.setCursor(0, 1);
-        lcd.print(`${member.user.username}`);
+        lcd.clear(); // Clear that screen
+        lcd.setCursor(0, 0); // set cursor
+        lcd.print(`New Member!`); // yeah
+        lcd.setCursor(0, 1); // mhmm
+        lcd.print(`${member.user.username}`); // display the username of the new member
+        led.on(); // turn LED on to alert
 
-        console.log(`New Member: ${member.user.username}`);
+        console.log(`New Member: ${member.user.username}`); // logging
 
-        setTimeout(() => {
-            LCD.refreshScreen();
-        }, 7000);
+        setTimeout(() => { // turn that guy off...
+            LCD.refreshScreen(); // refresh screen can be used because we don't know the index (technically the class does though), so this puts us back at THAT screen
+            led.off(); // Turn it back off
+        }, 7000); // after 7 seconds (in ms)
     }
 };
